3f3b734a6abe14cce94ab0b6994f3a14a58c833d
Abdulkadir Aydemir <aydemir_0225@hotmail.com>
2023-12-26 19:13:40 +0300

    new update

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -22,7 +22,6 @@ function ProductCard(props) {
     }
   };
 
-
   if (isDeleted) {
     return (
       <div className="card">
@@ -32,25 +31,58 @@ function ProductCard(props) {
     );
   }
 
+  const limitDescription = (text, limit) => {
+    const words = text.split(' ');
+    if (words.length > limit) {
+      return words.slice(0, limit).join(' ') + '...';
+    }
+    return text;
+  };
+
   return (
-    <div className="card">
+    <div
+      className="card"
+      style={{
+        width: "300px",
+        height: "400px",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <img
         src={props.product.thumbnail}
         className="card-img-top img-fluid"
         alt="..."
+        style={{ width: "100%", height: "200px", objectFit: "cover" }}
       />
-      <div className="card-body">
-        <h5 className="card-title">{props.product.title}</h5>
-        <p className="card-text">{props.product.description}</p>
-        <Link to={`/products/${props.product.id}`} className="btn btn-primary">
-          Detail
-        </Link>
-        <button
-          onClick={handleDelete}
-          style={{ marginLeft: 4 }}
-          className="btn btn-danger">
-          Sil
-        </button>
+      <div
+        className="card-body"
+        style={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <h5 className="card-title">{props.product.title}</h5>
+          <p className="card-text">{limitDescription(props.product.description, 10)}</p>
+        </div>
+        <div>
+          <Link
+            to={`/products/${props.product.id}`}
+            className="btn btn-primary"
+          >
+            Detay
+          </Link>
+          <button
+            onClick={handleDelete}
+            style={{ marginLeft: 4 }}
+            className="btn btn-danger"
+          >
+            Sil
+          </button>
+        </div>
       </div>
     </div>
   );
